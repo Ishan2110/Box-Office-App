331524fec7a51c727239b6fe02720113173f1391
ishan21 <guptaishan21@gmail.com>
2021-07-17 17:13:01 +0530

    displayed show results and small fetch refactoring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,57 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGET } from '../misc/Config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onInputChange = event => {
     setInput(event.target.value);
   };
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(resp => {
-        return resp.json();
-      })
-      .then(event => console.log(event));
+    apiGET(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onKeyDown = e => {
     if (e.keyCode === 13) {
-      fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-        .then(resp => {
-          return resp.json();
-        })
-        .then(event => console.log(event));
+      onSearch();
     }
   };
 
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   return (
-    <>
-      <MainPageLayout>
-        <input
-          type="text"
-          onChange={onInputChange}
-          onKeyDown={onKeyDown}
-          value={input}
-        />
-        <button type="button" onClick={onSearch}>
-          Search
-        </button>
-      </MainPageLayout>
-    </>
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
   );
 };
 
