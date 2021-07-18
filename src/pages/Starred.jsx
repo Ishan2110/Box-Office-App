56e77f1748f6d9064ac4a289699810f57a9591bc
ishan21 <guptaishan21@gmail.com>
2021-07-18 15:16:42 +0530

    finished starred page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
+import { apiGET } from '../misc/Config';
+import { useShows } from '../misc/Custom-hooks';
 
 const Starred = () => {
-  return <MainPageLayout>This is Starred12</MainPageLayout>;
+  const [starred] = useShows();
+
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      // We could have used a for loop for multiple fetch requests but instead we use Promise.all to do all processing concurrently.
+      const promises = starred.map(showId => apiGET(`shows/${showId}`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        // We do the above so that we don't receive error while calling ShowGrid in line 40 as it receives data in bit diff form
+        .then(results => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div>Shows are Still Loading</div>}
+      {error && <div>Error Occured:{error}</div>}
+      {!isLoading && !shows && <div>No shows were added</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
